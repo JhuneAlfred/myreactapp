d624d50cc89e7ad46262f34d6d427e49409b9308
JhuneAlfred <avelicaria@me.com>
2017-09-23 22:35:50 -0700

    Added modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducers, {forms} from './reducers'
+import createHistory from 'history/createBrowserHistory'
+import {Route} from 'react-router'
+import {
+  ConnectedRouter,
+  routerMiddleware,
+  routerReducer
+} from 'react-router-redux'
+import {combineForms, createForms} from 'react-redux-form'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory()
+const middleware = {routerMiddleware}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(combineReducers({
+  ...reducers,
+  router: routerReducer,
+  ...createForms(forms),
+}), /* preloadedState, */ composeEnhancers(
+  applyMiddleware(...middleware)
+));
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker();
---
+++ src/reducers.js
@@ -0,0 +1,12 @@
+//import cardLostStolen, {forms as cardLostStolenForms} from './cardLostStolen'
+// import travelNotifications from './travelNotifications'
+
+
+export const forms = {
+//  ...cardLostStolenForms
+}
+
+export default {
+//  cardLostStolen
+  // travelNotifications
+}
